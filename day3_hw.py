2cd6f2f6f617c63510d361da73b01f6633ffa552
ADOwen <a.einhorn886@gmail.com>
2021-11-18 16:56:03 -0500

    day 3 hw modified

--- day3_hw.py
+++ day3_hw.py
@@ -16,32 +16,38 @@ module.scouter(9001)
 
 # exercise 2
 
-# def shopping_cart():
-#     cart = {}
-#     while True:
-#         question = input(
-#             "Would you like to [S]how cart, [A]dd item [C]lear or [Q]uit? ")
-#         if question.lower() == 's':
-#             if cart:
-#                 print(cart)
-#             else:
-#                 print('Your cart is empty')
-#         if question.lower() == 'a':
-#             item = input(
-#                 "Please enter the item you would like to add to shopping cart: ")
-#             quantity = input("How many would you like? ")
-#             cart[item] = quantity
-#         if question.lower() == 'c':
-#             if cart:
-#                 cart.clear()
-#             else:
-#                 print("Your cart is empty")
-#         if question.lower() == 'q':
-#             print("Here's your final order, thanks for shopping with us")
-#             print('====================================================')
-#             for item, quantity in cart.items():
-#                 print(f"{quantity} {item.title()}")
-#             break
+def shopping_cart():
+    cart = {}
+    while True:
+        question = input(
+            "\nWould you like to [S]how cart, [A]dd item [C]lear or [Q]uit? ")
+        if question.lower() == 's':
+            if cart:
+                for item, quantity in cart.items():
+                    print(f"\n{quantity} : {item.title()}")
+            else:
+                print('\nYour cart is empty')
+        if question.lower() == 'a':
+            item = input(
+                "\nPlease enter the item you would like to add to shopping cart: ")
+            quantity = input("\nHow many would you like? ")
+            cart[item] = quantity
+        if question.lower() == 'c':
+            if cart:
+                cart.clear()
+                print("\nYour cart has been cleared")
+            else:
+                print("\nYour cart is empty")
+        if question.lower() == 'q':
+            if cart:
+                print("\nHere's your final order, thanks for shopping with us")
+                print('====================================================')
+                for item, quantity in cart.items():
+                    print(f"\n{quantity} : {item.title()}")
+                break
+            else:
+                print("\nSorry you couldn't find anything, have a great day!")
+                break
 
 
-# shopping_cart()
+shopping_cart()
